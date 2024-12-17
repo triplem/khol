9c88e744a8d30225894123ab793bda45381d90e9
Markus M. May <160079+triplem@users.noreply.github.com>
2024-12-17 15:10:02 +0100

    fix: remove automatic jdk download

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,9 +6,4 @@ dependencyResolutionManagement {
     }
 }
 
-plugins {
-    // Use the Foojay Toolchains plugin to automatically download JDKs required by subprojects.
-    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
-}
-
 rootProject.name = "khol"
